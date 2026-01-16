ce8aef5c7028be9511b3a4410bb108a1053798a2
Taghreed-EialSalman <tagreedcee@yahoo.com>
2026-01-15 19:40:19 -0500

    Rename module instance in tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-tt_um_<github>_nand user_project (
+tt_um_taghreed_eialsalman_nand user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
